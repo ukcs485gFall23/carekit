2e5f1096db2cc2ef5ff6f92b63c176d6c67c1c0b
akwaed <akwaed@gmail.com>
2023-11-02 08:21:29 -0400

    feat: Add more CareKit task cards

---
+++ OCKSample/Extensions/OCKHealthKitTask+Custom.swift .swift
@@ -0,0 +1,9 @@
+//
+//  OCKHealthKitTask+Custom.swift .swift
+//  OCKSample
+//
+//  Created by Elnoel Akwa on 11/2/23.
+//  Copyright © 2023 Network Reconnaissance Lab. All rights reserved.
+//
+
+import Foundation
---
+++ OCKSample/Models/CareKitCard.swift
@@ -0,0 +1,9 @@
+//
+//  CareKitCard.swift
+//  OCKSample
+//
+//  Created by Elnoel Akwa on 11/2/23.
+//  Copyright © 2023 Network Reconnaissance Lab. All rights reserved.
+//
+
+import Foundation
