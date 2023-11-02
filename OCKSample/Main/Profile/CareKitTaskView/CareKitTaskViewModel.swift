923b47ad2b347506de528288a32a8d509593f132
akwaed <akwaed@gmail.com>
2023-11-01 23:50:59 -0400

    add ability to specify cards

---
+++ OCKSample/Main/Profile/CareKitTaskView/CareKitTaskViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CareKitTaskViewModel.swift
+//  OCKSample
+//
+//  Created by Elnoel Akwa on 11/1/23.
+//  Copyright © 2023 Network Reconnaissance Lab. All rights reserved.
+//
+
+import Foundation
