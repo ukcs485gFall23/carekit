08d2b5e5f8369e9cc00febb47d2e01a08c4993cc
Elnoel Akwa <37512610+akwaed@users.noreply.github.com>
2023-11-02 10:09:48 -0400

    Revert "feat: Add more CareKit task cards (#3)"
    This reverts commit 359f4cfb965b49b927764271837a90d89b2d4ca3.

--- OCKSample/Extensions/OCKTask+Custom.swift
+++
@@ -1,9 +0,0 @@
-//
-//  OCKTask+Custom.swift
-//  OCKSample
-//
-//  Created by Elnoel Akwa on 11/2/23.
-//  Copyright © 2023 Network Reconnaissance Lab. All rights reserved.
-//
-
-import Foundation
--- OCKSample/Main/Profile/CareKitTaskView/CareKitTaskView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CareKitTaskView.swift
-//  OCKSample
-//
-//  Created by Elnoel Akwa on 11/2/23.
-//  Copyright © 2023 Network Reconnaissance Lab. All rights reserved.
-//
-
-import Foundation
