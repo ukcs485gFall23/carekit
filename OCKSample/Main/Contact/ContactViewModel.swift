550c450b50c0056cd866884e882858ae14196c5b
akwaed <akwaed@gmail.com>
2023-11-07 11:13:51 -0500

    feat: Improve Profile/Contact View

--- OCKSample/Main/Contact/ContactViewModel.swift
+++ OCKSample/Main/Contact/ContactViewModel.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2023 Network Reconnaissance Lab. All rights reserved.
 //
 
+import CareKitStore
 import Foundation
+
+class ContactViewModel: ObservableObject {
+
+    static func contactQuery() -> OCKContactQuery {
+
+        let query = OCKContactQuery(for: Date())
+        // BAKER: Appears to be a bug in CareKit, commenting these out for now
+        // query.sortDescriptors.append(.familyName(ascending: true))
+        // query.sortDescriptors.append(.givenName(ascending: true))
+
+        return query
+    }
+
+}
