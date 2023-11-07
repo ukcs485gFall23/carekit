e327357f6edb6266247aca456e55b612cd6ada62
akwaed <akwaed@gmail.com>
2023-11-07 11:13:26 -0500

    feat: Improve Profile/Contact View

---
+++ OCKSample/Main/Contact/ContactViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ContactViewModel.swift
+//  OCKSample
+//
+//  Created by Elnoel Akwa on 11/7/23.
+//  Copyright © 2023 Network Reconnaissance Lab. All rights reserved.
+//
+
+import Foundation
