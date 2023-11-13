70cbb46c532b3a9cf2cd58cee2d440926f38a500
akwaed <akwaed@gmail.com>
2023-11-13 01:54:34 -0500

    feat: add my contact view

---
+++ OCKSample/Main/Profile/MyContact/MyContactViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MyContactViewController.swift
+//  OCKSample
+//
+//  Created by Elnoel Akwa on 11/13/23.
+//  Copyright © 2023 Network Reconnaissance Lab. All rights reserved.
+//
+
+import Foundation
