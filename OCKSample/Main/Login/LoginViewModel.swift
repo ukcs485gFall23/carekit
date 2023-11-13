e3090f0efd07a67985654c50c99d036c958193c4
akwaed <akwaed@gmail.com>
2023-11-13 01:45:47 -0500

    feat:add Patient for CarePlan

--- OCKSample/Main/Login/LoginViewModel.swift
+++ OCKSample/Main/Login/LoginViewModel.swift
@@ -124,6 +124,16 @@ class LoginViewModel: ObservableObject {
                                     familyName: lastName)
         newPatient.userType = type
         let savedPatient = try await appDelegate.store.addPatient(newPatient)
+        // Added code to create a contact for the respective signed up user
+        let newContact = OCKContact(id: remoteUUID.uuidString,
+                                    name: newPatient.name,
+                                    carePlanUUID: nil)
+
+        // This is new contact that has never been saved before
+        _ = try await appDelegate.store.addAnyContact(newContact)
+        // TODO: You need to handle tying a patient to all of your
+        // CarePlans, how would you do it here since you have a
+        // a saved patient uuid?
         try await appDelegate.store.populateSampleData()
         try await appDelegate.healthKitStore.populateSampleData()
         appDelegate.parseRemote.automaticallySynchronizes = true
---
+++ OCKSample/Models/CarePlanID.swift
@@ -0,0 +1,16 @@
+//
+//  CarePlanID.swift
+//  OCKSample
+//
+//  Created by Elnoel Akwa on 11/13/23.
+//  Copyright © 2023 Network Reconnaissance Lab. All rights reserved.
+//
+
+import Foundation
+
+// TODO: Add CarePlans specific to your app here.
+// If you don't remember what a OCKCarePlan is, read the CareKit docs.
+enum CarePlanID: String {
+    case health // Add custom id's for your Care Plans, these are examples
+    case checkIn
+}
